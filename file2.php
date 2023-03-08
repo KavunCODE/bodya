942b12c49b06768244c977654e40a698c09b31f7
bodya <4restline@gmail.com>
2023-03-08 12:35:19 +0100

    modified

---
+++ file2.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+<head>
+    <title>Our Funky HTML Page</title>
+    <meta name="description" content="Our first page">
+    <meta name="keywords" content="html tutorial template">
+    <title>Dołączanie plików</title>
+</head>
+<body>
+<h4>Początek strony</h4>
+<?php include "./skrypt/lista.php" //include, include_once, require, require_once ?>
+<?php include_once "./skrypt/lista.php" ?>
+<?php require "./skrypt/lista.php";
+require_once "./skrypt/lista.php"?>
+
+
+<h4>Koniec strony</h4>
+</body>
+</html>
---
+++ zajecia_2.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+<head>
+    <title>Our Funky HTML Page</title>
+    <meta name="description" content="Our first page">
+    <meta name="keywords" content="html tutorial template">
+</head>
+<body>
+<ul>
+  <li>
+      <ol>
+          <li></li>>
+          <li></li>
+          <li></li>
+      </ol>
+  </li>
+</ul>
+</body>
+</html>
