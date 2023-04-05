49aa2025c8f745d49840387efe7ac3e485f66edf
bodyan4eg <98308682+bodyan4eg@users.noreply.github.com>
2023-04-05 13:46:35 +0200

    Add files via upload

--- wsb_dIinz_2_K15_Inf-main/scripts/add_user.php
+++ wsb_dIinz_2_K15_Inf-main/scripts/add_user.php
@@ -1,22 +1,43 @@
 <?php
-foreach ($_POST as $key => $value){
-	//echo "$key: $value<br>";
-	if(empty($value)){
-		$_SESSION["error"] = "Wypelnij wszystkie pola w formularzu!";
-		echo "<script>history.back();</script>";
-		exit();
+	session_start();
+	// print_r($_POST);
+	
+	foreach ($_POST as $key => $value){
+		// echo "$key: $value<br>";
+		if (empty($value)){
+			// echo "$key<br>";
+			$_SESSION["error"] = "Wypełnij wszystkie pola w formularzu!";
+			echo "<script>history.back();</script>";
+			exit();
+		}
 	}
-}
 
-require_once "./connect.php";
-$sql ="INSERT INTO `users` (`id`, `city_id`, `firstName`, `lastName`, `birthday`) VALUES (NULL, '$_POST[city_id]', '$_POST[firstName]', '$_POST[lastName]', '$_POST[birthday]');";
-$conn->query($sql);
+		if (!isset($_POST["term"]))
+	{
+		$_SESSION["error"] = "Zatwierdź regulamin!";
+		echo "<script>history.back();</script>";
+		exit();	
+	}
 
-//echo $conn->affected_rows;
-if ($conn->affected_rows == 1){
-	$_SESSION["error"] = "Prawidlowo dodano rekord";
-}else{
-	$_SESSION["error"] = "Nie dodano rekoru";
-}
+	require_once "./connect.php";
+	$sql = "INSERT INTO `users` (`id`, `city_id`, `firstName`, `lastName`, `birthday`) VALUES (NULL, '$_POST[city_id]', '$_POST[firstName]', '$_POST[lastName]', '$_POST[birthday]');";
+	$conn->query($sql);
 
-header("location: ../4_db/3_db_table.php");
+	// echo $conn->affected_rows;
+	if ($conn->affected_rows == 1){
+		// echo "Prawidłowo dodano rekord";
+		$_SESSION["error"] = "Prawidłowo dodano rekord";
+	}else{
+		// echo "Nie dodano rekordu!";
+		$_SESSION["error"] = "Nie dodano rekordu!";
+	}
+
+	header("location: ../4_db/5_db_table_add_update.php");
+
+
+
+
+
+
+
+
--- wsb_dIinz_2_K15_Inf-main/scripts/delete_user.php
+++ wsb_dIinz_2_K15_Inf-main/scripts/delete_user.php
@@ -12,7 +12,8 @@
 //		echo "Nie usunięto rekordu!";
 		$deleteUser = 0;
 	}
-	header("location: ../4_db/3_db_table.php?deleteUser=$deleteUser");
+//	header("location: ../4_db/3_db_table.php?deleteUser=$deleteUser");
+	header("location: ../4_db/5_db_table_add_update.php?deleteUser=$deleteUser");
 	?>
 
 <script>
